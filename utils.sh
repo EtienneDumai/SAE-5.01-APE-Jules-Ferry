0d9acf91ecade5ed727047cbbe3c6bcf908550f4
EtienneDumai <etienne.dumai@gmail.com>
2025-12-10 17:35:00 +0100

    modif du script utils.sh

--- utils.sh
+++ utils.sh
@@ -3,12 +3,33 @@ show_menu() {
     echo "=============================="
     echo "        MENU UTILS"
     echo "=============================="
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    echo "       Frontend Angular       "
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
     echo " 1) Lancer le front en mode dev"
     echo " 2) Lancer le back en mode production"
+    echo " 3) Generer un composant Angular"
+    echo " 4) Generer un service Angular"
     echo "------------------------------"
     echo " 0) Quitter"
     echo "=============================="
     echo -n "Votre choix : "
+}
+generate_component() {
+    echo -n "Entrez le nom du composant Angular à générer : "
+    read component_name
+    cd frontend && ng generate component components/"$component_name" && cd ..
+    echo "Composant '$component_name' généré avec succès."
+    echo "Appuyez sur une touche pour continuer..."
+    read -n 1
+}
+generate_service() {
+    echo -n "Entrez le nom du service Angular à générer : "
+    read service_name
+    cd frontend && ng generate service services/"$service_name"/"$service_name" && cd ..
+    echo "Service '$service_name' généré avec succès."
+    echo "Appuyez sur une touche pour continuer..."
+    read -n 1
 }
 while true; do
     show_menu
@@ -20,6 +41,12 @@ while true; do
             ;;
         2)
             cd frontend && ng build --configuration=production && ng serve --configuration=production && cd ..
+            ;;
+        3)
+            generate_component
+            ;;
+        4)
+            generate_service
             ;;
         0)
             echo "Au revoir!"
