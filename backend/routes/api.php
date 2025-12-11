d6f05b140e4464942ec3837cdcc2b4184d9f77fa
EtienneDumai <etienne.dumai@gmail.com>
2025-12-11 21:22:12 +0100

    ajout de tout les controller necessaire

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,5 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ActualiteController;
+use App\Http\Controllers\CreneauController;
+use App\Http\Controllers\EvennementController;
+use App\Http\Controllers\FormulaireController;
+use App\Http\Controllers\InscriptionController;
+use App\Http\Controllers\TacheController;
+use App\Http\Controllers\UtilisateurController;
 
-Route::get('actualites', [ActualiteController::class]);
+Route::apiResource('actualites', ActualiteController::class);
+Route::apiResource('creneaux', CreneauController::class);
