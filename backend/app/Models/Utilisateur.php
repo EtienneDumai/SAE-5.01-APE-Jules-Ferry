47d753b97d04dd0f5bd40e68f1e67c72310a2f80
Rémi GENTIL <rgentil@domaine.local>
2025-12-12 08:59:08 +0100

    Changement du User.php pour Utilisateur.php et avoir une config propre

--- backend/app/Models/Utilisateur.php
+++ backend/app/Models/Utilisateur.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Utilisateur extends Model
+
+class Utilisateur extends Authenticatable
 {
     use HasFactory;
 
