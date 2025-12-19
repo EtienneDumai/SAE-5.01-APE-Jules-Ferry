62978152f77f2ad35da91b9854750cbcfff56d44
EtienneDumai <etienne.dumai@gmail.com>
2025-12-19 21:22:46 +0100

    Ajout composant card evnnement actualite

---
+++ frontend/src/app/components/actualite-card/actualite-card.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { StatutActualite } from '../../enums/StatutActualite/statut-actualite';
+
+@Component({
+  selector: 'app-actualite-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './actualite-card.component.html',
+  styleUrl: './actualite-card.component.css'
+})
+export class ActualiteCardComponent {
+  @Input() titre!: string;
+  @Input() contenu!: string;
+  @Input() imageUrl!: string;
+  @Input() datePublication!: Date;
+  @Input() statut!: StatutActualite;
+
+}
---
+++ frontend/src/app/components/evennement-card/evennement-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-evennement-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './evennement-card.component.html',
+  styleUrl: './evennement-card.component.css'
+})
+export class EvennementCardComponent {
+
+}
