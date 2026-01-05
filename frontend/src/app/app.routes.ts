8e4eaab18c33bbda035c8e6994dd64fc67f6b4c9
EtienneDumai <etienne.dumai@gmail.com>
2026-01-05 11:15:43 +0100

    modification des routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -21,9 +21,9 @@ export const routes: Routes = [
         canActivate: [guestGuard]
     },
     
-    { path: '**', redirectTo: '' },
-    { path:'actualites', component: ActualitePageComponent },
-    { path: 'actualites/:id', component: ActualiteDetailComponent },
-    { path: 'evenements', component: EvenementPageComponent },
-    { path: 'evenements/:id', component: EvenementDetailComponent }
+    { path: '', loadComponent: () => import('./pages/accueil/accueil.component').then(m => m.AccueilComponent) },
+    { path: 'actualites', loadComponent: () => import('./pages/actualite-page/actualite-page.component').then(m => m.ActualitePageComponent) },
+    { path: 'actualites/:id', loadComponent: () => import('./pages/actualite-detail/actualite-detail.component').then(m => m.ActualiteDetailComponent) },
+    { path: 'evenements', loadComponent: () => import('./pages/evenement-page/evenement-page.component').then(m => m.EvenementPageComponent) },
+    { path: 'evenements/:id', loadComponent: () => import('./pages/evenement-detail/evenement-detail.component').then(m => m.EvenementDetailComponent) }
 ];
