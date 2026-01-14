ef101db185979e5af3bc499bada340d6cd4d0656
Etienne Dumai <etienne.dumai@gmail.com>
2026-01-14 23:21:08 +0100

    ajout du composant form d'inscription à un evenement

---
+++ frontend/src/app/components/forms/form-inscription-evenement/form-inscription-evenement.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-inscription-evenement',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-inscription-evenement.component.html',
+  styleUrl: './form-inscription-evenement.component.css'
+})
+export class FormInscriptionEvenementComponent {
+
+}
