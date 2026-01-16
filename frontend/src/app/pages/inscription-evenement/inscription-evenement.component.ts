8d5e900cc025a295e40e28af9d0f8ae2133a63dc
Jules VINET LATRILLE <julesvinet64@gmail.com>
2026-01-16 14:57:14 +0100

    Test accueil : TOTAL: 8 SUCCESS

--- frontend/src/app/pages/inscription-evenement/inscription-evenement.component.ts
+++ frontend/src/app/pages/inscription-evenement/inscription-evenement.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { Tache } from '../../models/Tache/tache';
 import { TacheService } from '../../services/Tache/tache.service';
-import { EvennementService } from '../../services/Evennement/evennement.service';
+import { EvenementService } from '../../services/Evenement/evenement.service';
 import { ActivatedRoute } from '@angular/router';
 import { ToastService } from '../../services/Toast/toast.service';
 import { TypeErreurToast } from '../../enums/TypeErreurToast/type-erreur-toast';
@@ -18,7 +18,7 @@ export class InscriptionEvenementComponent implements OnInit {
   listeTaches: Tache[] = [];
   evenement!: Evenement;
   private readonly tacheService = inject(TacheService);
-  private readonly evenementService = inject(EvennementService);
+  private readonly evenementService = inject(EvenementService);
   private readonly route: ActivatedRoute = inject(ActivatedRoute);
   private readonly toastService = inject(ToastService);
   ngOnInit(): void {
@@ -32,11 +32,11 @@ export class InscriptionEvenementComponent implements OnInit {
         this.toastService.show('Erreur lors du chargement des tâches de l\'événement', TypeErreurToast.ERROR);
       }
     });
-    this.evenementService.getEvennementById(Number(id_evennement)).subscribe({
-      next: (data) => {
+    this.evenementService.getEvenementById(Number(id_evennement)).subscribe({
+      next: (data: Evenement) => {
         this.evenement = data;
       },
-      error: (err) => {
+      error: (err: any) => {
         console.error(err);
         this.toastService.show('Erreur lors du chargement de l\'événement', TypeErreurToast.ERROR);
       }
