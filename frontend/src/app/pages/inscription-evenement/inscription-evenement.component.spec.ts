5bb091f7ee2004404397d9cd1b0bec24336a24c0
jvlatrille <julesvinet64@gmail.com>
2026-01-28 15:03:22 +0100

    test front de l'inscription aux événements

--- frontend/src/app/pages/inscription-evenement/inscription-evenement.component.spec.ts
+++ frontend/src/app/pages/inscription-evenement/inscription-evenement.component.spec.ts
@@ -1,15 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { InscriptionEvenementComponent } from './inscription-evenement.component';
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
-import { provideRouter } from '@angular/router';
+import { provideRouter, ActivatedRoute } from '@angular/router';
+import { of, throwError } from 'rxjs';
+import { TacheService } from '../../services/Tache/tache.service';
+import { EvenementService } from '../../services/Evenement/evenement.service';
+import { ToastService } from '../../services/Toast/toast.service';
+import { TypeErreurToast } from '../../enums/TypeErreurToast/type-erreur-toast';
 import { Evenement } from '../../models/Evenement/evenement';
+import { Tache } from '../../models/Tache/tache';
 import { StatutEvenement } from '../../enums/StatutEvenement/statut-evenement';
 
 describe('InscriptionEvenementComponent', () => {
   let component: InscriptionEvenementComponent;
   let fixture: ComponentFixture<InscriptionEvenementComponent>;
+  let tacheServiceSpy: jasmine.SpyObj<TacheService>;
+  let evenementServiceSpy: jasmine.SpyObj<EvenementService>;
+  let toastServiceSpy: jasmine.SpyObj<ToastService>;
 
   const mockEvenement: Evenement = {
     id_evenement: 1,
@@ -25,26 +33,74 @@ describe('InscriptionEvenementComponent', () => {
     id_formulaire: 1
   };
 
+  const mockTaches: Tache[] = [
+    {
+      id_tache: 1,
+      nom_tache: 'Tache 1',
+      description: 'Description 1',
+      heure_debut_globale: '10:00',
+      heure_fin_globale: '11:00',
+      id_formulaire: 1,
+      creneaux: []
+    }
+  ];
+
   beforeEach(async () => {
+    tacheServiceSpy = jasmine.createSpyObj('TacheService', ['getAlltachesByIdEvennement']);
+    evenementServiceSpy = jasmine.createSpyObj('EvenementService', ['getEvenementById']);
+    toastServiceSpy = jasmine.createSpyObj('ToastService', ['show']);
+
+    tacheServiceSpy.getAlltachesByIdEvennement.and.returnValue(of(mockTaches));
+    evenementServiceSpy.getEvenementById.and.returnValue(of(mockEvenement));
+
     await TestBed.configureTestingModule({
       imports: [InscriptionEvenementComponent],
       providers: [
         provideHttpClient(),
         provideHttpClientTesting(),
-        provideRouter([])
+        provideRouter([]),
+        { provide: TacheService, useValue: tacheServiceSpy },
+        { provide: EvenementService, useValue: evenementServiceSpy },
+        { provide: ToastService, useValue: toastServiceSpy },
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: {
+                get: () => '1'
+              }
+            }
+          }
+        }
       ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(InscriptionEvenementComponent);
     component = fixture.componentInstance;
-    
-    component.evenement = mockEvenement;
-    
     fixture.detectChanges();
   });
 
   it('devrait créer', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('devrait charger les tâches et l\'événement à l\'initialisation', () => {
+    expect(tacheServiceSpy.getAlltachesByIdEvennement).toHaveBeenCalledWith(1);
+    expect(evenementServiceSpy.getEvenementById).toHaveBeenCalledWith(1);
+    expect(component.listeTaches).toEqual(mockTaches);
+    expect(component.evenement).toEqual(mockEvenement);
+  });
+
+  it('devrait afficher une erreur si le chargement des tâches échoue', () => {
+    tacheServiceSpy.getAlltachesByIdEvennement.and.returnValue(throwError(() => new Error('Erreur')));
+    component.ngOnInit();
+    expect(toastServiceSpy.show).toHaveBeenCalledWith('Erreur lors du chargement des tâches de l\'événement', TypeErreurToast.ERROR);
+  });
+
+  it('devrait afficher une erreur si le chargement de l\'événement échoue', () => {
+    evenementServiceSpy.getEvenementById.and.returnValue(throwError(() => new Error('Erreur')));
+    component.ngOnInit();
+    expect(toastServiceSpy.show).toHaveBeenCalledWith('Erreur lors du chargement de l\'événement', TypeErreurToast.ERROR);
   });
 });
