512d6405bb688f02f1da21fd48862d1e6c981dcf
jvlatrille <julesvinet64@gmail.com>
2026-01-28 15:51:17 +0100

    Ajout des tests unitaires CRUD pour FormulaireController et création de la factory associée

---
+++ backend/database/factories/FormulaireFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Formulaire;
+use App\Models\Utilisateur;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class FormulaireFactory extends Factory
+{
+    protected $model = Formulaire::class;
+
+    public function definition(): array
+    {
+        return [
+            'nom_formulaire' => $this->faker->sentence(),
+            'description' => $this->faker->paragraph(),
+            'statut' => 'actif',
+            'id_createur' => Utilisateur::factory(),
+        ];
+    }
+}
