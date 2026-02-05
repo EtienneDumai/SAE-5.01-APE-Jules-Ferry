46c906910c3ea2a4ba31aaaf053580d3c3421444
mouhakeita23@gmail.com <mouhakeita23@gmail.com>
2026-02-05 11:29:55 +0100

    Temps d'apparition des toast ok

--- frontend/src/app/services/Toast/toast.service.ts
+++ frontend/src/app/services/Toast/toast.service.ts
@@ -8,8 +8,15 @@ import {TypeToast} from '../../models/TypeToast/type-toast';
 export class ToastService {
   private toastSubject = new BehaviorSubject<TypeToast | null>(null);
   toast = this.toastSubject.asObservable();
+  private toastTimeout: ReturnType<typeof setTimeout> | null = null;
   show(message: string, type: TypeErreurToast = TypeErreurToast.SUCCESS): void {
     this.toastSubject.next({message, type});
+    if (this.toastTimeout) {
+      clearTimeout(this.toastTimeout);
+    }
+    this.toastTimeout = setTimeout(() => {
+      this.clear();
+    }, 5000);
   }
   clear(): void {
     this.toastSubject.next(null);
