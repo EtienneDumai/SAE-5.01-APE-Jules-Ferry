32bfcb94367a68704ae3c206d6378acc358fcc0e
EtienneDumai <etienne.dumai@gmail.com>
2026-03-30 14:45:19 +0200

    ajout des __dir__

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -3,11 +3,11 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(DIR))
+return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: DIR . '/../routes/web.php',
-        api: DIR . '/../routes/api.php',
-        commands: DIR . '/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )->withMiddleware(function (Middleware $middleware) {
 
