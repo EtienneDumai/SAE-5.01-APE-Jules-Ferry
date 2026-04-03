b0417acaab54d240b307a97e70a98381e3ccbae9
Thibault CHIPY <chipythibault@icloud.com>
2026-04-03 15:27:25 +0200

    ajout des liens

--- frontend/src/app/pages/a-propos/a-propos.component.ts
+++ frontend/src/app/pages/a-propos/a-propos.component.ts
@@ -10,11 +10,11 @@ import { CommonModule } from '@angular/common';
 })
 export class AProposComponent {
   teamMembers = [
-    { name: 'Rémi GENTIL', role: 'Proxy Product Owner / Développeur', website: 'https://github.com/', linkedin: 'https://www.linkedin.com/in/remi-gentil/' },
+    { name: 'Rémi GENTIL', role: 'Proxy Product Owner / Développeur', website: 'https://remi-gentil.vercel.app/', linkedin: 'https://www.linkedin.com/in/remi-gentil/' },
     { name: 'Etiene DUMAI', role: 'Scrum Master / Développeur', website: 'https://github.com/', linkedin: 'https://www.linkedin.com/in/etienne-dumai/' },
-    { name: 'Théo LAGUE', role: 'Développeur', website: 'https://github.com/', linkedin: 'https://www.linkedin.com/in/th%C3%A9o-lag%C3%BCe-500503268/' },
+    { name: 'Théo LAGÜE', role: 'Développeur', website: 'https://portfolio-three-mu-50.vercel.app/', linkedin: 'https://www.linkedin.com/in/th%C3%A9o-lag%C3%BCe-500503268/' },
     { name: 'Jules VINET-LATRILLE', role: 'Développeur', website: 'https://julesvinetlatrille.org/', linkedin: 'https://www.linkedin.com/in/julesvinetlatrille/' },
     { name: 'Mouhamadou Moussa KEITA', role: 'Développeur', linkedin: 'https://www.linkedin.com/in/mouhamadou-moussa-keita-ba73b02b8/' },
-    { name: 'Thibault CHIPY', role: 'Développeur', website: 'https://github.com/', linkedin: 'https://www.linkedin.com/in/thibault-chipy/' }
+    { name: 'Thibault CHIPY', role: 'Développeur', website: 'https://thibault-chipy.github.io/portfolio/', linkedin: 'https://www.linkedin.com/in/thibault-chipy/' }
   ];
 }
